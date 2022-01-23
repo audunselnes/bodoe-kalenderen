7af85662046b4e220741d1cf7adf96a8fd3e219e
Emilie Dybendal Aulie <59264023+emilieaulie@users.noreply.github.com>
2022-01-23 12:07:04 +0100

    fetchData

--- src/api/requestData.js
+++ src/api/requestData.js
@@ -2,40 +2,14 @@ const cheerio= require("cheerio");
 const axios= require("axios");
 
 const axiosConfig = {
-    url: "https://www.whateverorigin.org/get?url=" + encodeURIComponent("https://tikkio.com/in/bodo")
+    url: "/scrape",
     withCredentials: false,
     origin: "*",
     headers: {'Access-Control-Allow-Origin': '*'}
 }
 
 const getEventData = async () => 
-    await axios(
-        //"https://tikkio.com/in/bodo", {headers: {
-        //'Access-Control-Allow-Origin': '*',
-        //"withCredentials": true
-      //}}
-      axiosConfig
-      ).then(res => {
-        const $ = cheerio.load(res.data);
-        const data= [];
-        $(".top-event").map((index, element) => {
-            const link= $(element)
-                .attr("href");
-            const title= $(element)
-                .find(".title")
-                .attr("title");
-            const description= $(element)
-                .find(".location")
-                .text();
-            const image= $(element)
-                .find(".image")
-                .attr("data-src");
-            data.push({link, title, description, image});
-            
-        })
-        return data;
-    });
+   await axios(axiosConfig).then(a => a.data);
 
-    getEventData().then(a=> console.log(a));
 
-    export default getEventData;
+export default getEventData;
