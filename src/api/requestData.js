e90e74c30ec7f3aad6411e5789631fac470f1e5e
André Schjøth <aschjoth@gmail.com>
2022-01-24 18:48:09 +0100

    feat & chore: firebase hosting and cleanup

--- src/api/requestData.js
+++ src/api/requestData.js
@@ -1,15 +1,9 @@
-const cheerio= require("cheerio");
-const axios= require("axios");
+const axios = require("axios");
 
 const axiosConfig = {
-    url: process.env.REACT_APP_IS_DEV == "true" ? "/scrape": "https://us-central1-bodokal.cloudfunctions.net/scrape",
-    withCredentials: false,
-    origin: "*",
-    headers: {'Access-Control-Allow-Origin': '*'}
-}
+	url: "https://us-central1-bodokal.cloudfunctions.net/scrape",
+};
 
-const getEventData = async () => 
-   await axios(axiosConfig).then(a => a.data);
-
+const getEventData = async () => await axios(axiosConfig).then((a) => a.data);
 
 export default getEventData;
